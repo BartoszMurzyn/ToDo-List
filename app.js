6f5128f556188b815b32870a17b08a91b01ca6ba
Bartosz Murzyn <bartoszmurzyn@Barteks-MacBook-Air.local>
2023-07-12 12:25:10 +0200

    Change var for let

--- app.js
+++ app.js
@@ -4,36 +4,32 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-var items = [];
-
 app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-    
-    var day = new Date();
+let items = []
 
-    var options = {
+app.get("/", function (req, res) {
+
+    let day = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var date = day.toLocaleDateString("en-US", options);
+    let date = day.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: date, newListItems: items});
+    res.render('list', {kindOfDay: date, newItemsList:items});
 });
-
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
-    console.log(req.body.newItems);
     console.log(items);
     res.redirect("/");
-});
+})
 
-
-
-app.listen(3000, function() {
-    console.log("Server working on port 3000");
+app.listen(3000, function () {
+    console.log("Server working");
 });
